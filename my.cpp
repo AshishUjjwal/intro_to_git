9396417860a94f56488c5b4c4c0fb695ac47f63c
Ashish Ujjwal <ashishfrommohania@gamil.com>
2023-12-06 15:16:14 +0530

    2nd commit

---
+++ my.cpp
@@ -0,0 +1,27 @@
+// This Program is done by Ashish Ujjwal
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define pb push_back
+#define all(X) (X).begin(), (X).end()
+#define np cout << -1 << endl
+#define rn return
+
+void solve(){
+    cout<<"Hello world"<<endl;
+    cout<<"Hello world"<<endl;
+    cout<<"Hello world"<<endl;
+    cout<<"Hello world"<<endl;
+}
+
+int main(){
+
+    ll t; cin>>t;
+    while(t--){
+       solve();
+    }
+    return 0;
+}
